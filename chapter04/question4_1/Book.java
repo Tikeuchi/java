8e21619e562130ae28a4f210d4607a635360a75b
study-re-kailum <65376292+study-re-kailum@users.noreply.github.com>
2020-07-16 16:35:37 +0900

    update chapter04

--- chapter04/question4_1/Book.java
+++ chapter04/question4_1/Book.java
@@ -6,39 +6,49 @@ public class Book implements Comparable<Book>, Cloneable {
 	private String title;
 	private Date publishDate;
 	private String comment;
+	// (1) HashSetに格納する処理
 	public int hashCode() {
 		int r = 1;
-		r = 31 * r + publishDate.hashCode();
-		r = 31 * r + title.hashCode();
+		r = 31 * r + /* 発行日をハッシュ化したもの */;
+		r = 31 * r + /* 書名をハッシュ化したもの */;
 		return r;
 	}
+	// (1) 書名と発行日が同じであるか判定する
 	public boolean equals(Object o) {
-		if(this == o) {
+		// そのオブジェクトが自分自身ならtrue
+		if( /* 条件を記述 */ ) {
 			return true;
 		}
-		if(o == null) {
+		// そのオブジェクトがnullならfalse
+		if( /* 条件を記述 */ ) {
 			return false;
 		}
-		if(!(o instanceof Book)) {
+		// そのオブジェクトがBookクラスのインスタンスでなければfalse
+		if( /* 条件を記述 */ ) {
 			return false;
 		}
 		Book b = (Book) o;
-		if(!publishDate.equals(b.publishDate)) {
+		// 発行日が同じでなければfalse
+		if( /* 条件を記述 */ ) {
 			return false;
 		}
-		if(!title.equals(b.title)) {
+		// 書名が同じでなければfalse
+		if( /* 条件を記述 */ ) {
 			return false;
 		}
+		// 全ての条件をクリアしたらtrue
 		return true;
 	}
+	// (2) Bookインスタンスの発行日をcompareToメソッドで比較する
 	public int compareTo(Book o) {
-		return this.publishDate.compareTo(o.publishDate);
+		return ;
 	}
+	// (3) clone()を呼び出すと、深いコピーによる複製が行われる
 	public Book clone() {
 		Book b = new Book();
-		b.title = this.title;
-		b.comment = this.comment;
-		b.publishDate = (Date) this.publishDate.clone();
+
+
+
 		return b;
 	}
 	// フィールド宣言、getter/setterの宣言は省略
